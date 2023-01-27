908f9a866494b0852b50da6b20d94a8add082e03
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2023-01-28 01:40:52 +0200

    create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 // scss
 import { Link } from 'react-router-dom';
+import Button from '../elements/Button';
 import Logo from '../elements/Logo';
 import styles from '../styles/components/footer.module.scss';
 import links from '../styles/elements/link.module.scss';
@@ -8,28 +9,40 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        {/* logo */}
-        <Logo />
-        {/* Navigation */}
-        <div className={styles.block}>
-          <navigation className={styles.nav}>
-            <Link className={links.link} to="/">
-              <span>Home</span>
-            </Link>
-            <Link className={links.link} to="/blogs">
-              <span>Blog</span>
-            </Link>
-            <Link className={links.link} to="/about-us">
-              <span>About Us</span>
-            </Link>
-            <Link className={links.link} to="/contact">
-              <span>Contact Us</span>
-            </Link>
-            <Link className={links.link} to="/privacy-policy">
-              <span>Privacy Policy</span>
-            </Link>
-          </navigation>
+        {/* Top */}
+        <div className={styles.sectionTop}>
+          {/* logo */}
+          <Logo />
+          {/* Navigation */}
+          <div className={styles.block}>
+            <navigation className={styles.nav}>
+              <Link className={links.link} to="/">
+                <span>Home</span>
+              </Link>
+              <Link className={links.link} to="/blogs">
+                <span>Blog</span>
+              </Link>
+              <Link className={links.link} to="/about-us">
+                <span>About Us</span>
+              </Link>
+              <Link className={links.link} to="/contact">
+                <span>Contact Us</span>
+              </Link>
+              <Link className={links.link} to="/privacy-policy">
+                <span>Privacy Policy</span>
+              </Link>
+            </navigation>
+          </div>
         </div>
+        {/* Center */}
+        <div className={styles.sectionCenter}>
+          <span className={styles.text}>
+            Subscribe to our news letter to get latest updates and news
+          </span>
+          <input type="text" className={styles.input} />
+          <Button />
+        </div>
+        <div className={styles.sectionBottom}></div>
       </div>
     </footer>
   );
